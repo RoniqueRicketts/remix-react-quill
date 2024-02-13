9985bcbc1f10b19fa37ec84a86a3dfd277ce2ded
Ronique <roniquericketts10@gmail.com>
2024-02-12 20:20:33 -0500

    initialize app

---
+++ app/routes/_index.tsx
@@ -0,0 +1,41 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix React-Quill Solution</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://www.rickettstech.com"
+            rel="noreferrer"
+          >
+            Meet the engineers.
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
